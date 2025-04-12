bca05e35c5f8368723407d415bc758a6b6c79fea
baskak <andrbaskov@gmail.com>
2025-04-12 21:19:03 +0500

    теперь календарь кликабельный
    # Conflicts:
    #	README.md

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -3,22 +3,21 @@ package com.example.myapplication.ui.home
 import TaskDecorator
 import android.os.Bundle
 import android.view.LayoutInflater
-import com.example.myapplication.R
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentHomeBinding
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import android.content.Intent
+import com.example.myapplication.DateActivity
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -39,6 +38,16 @@ class HomeFragment : Fragment() {
         )
 
         calendarView.addDecorator(TaskDecorator(taskDates))
+
+        // Установим слушатель на выбор даты
+        calendarView.setOnDateChangedListener { widget, date, selected ->
+            if (selected) {
+                // Передаем выбранную дату в новый фрагмент или активность
+                val intent = Intent(requireContext(), DateActivity::class.java)
+                intent.putExtra("selectedDate", date.toString())
+                startActivity(intent)
+            }
+        }
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
